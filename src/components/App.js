0c31d16915b552fdc928f64f431a4c4648428f2b
Kadeem140 <Beckford.kadeem@yahoo.com>
2022-08-12 20:38:43 -0400

    MVP

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+ function App(){
+
+    const [dog, setDog] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res, "Fetch Data")
+                setDog(res)
+                setIsLoaded(true)
+            });
+
+    }, []);
+
+    if(!isLoaded) return <p>Loading...</p>;
+
+    return (
+        <div>
+            <h1>{dog.message}</h1>
+
+        </div>
+    )
+}
+
+export default App;
